3c6a14ab4917bd3fc6f9c9a562e76095f58f74b6
Tanilo Vulcao <tanilovulcao@gmail.com>
2026-06-12 08:43:05 -0300

    alteração da migrate owners(proprietario)

--- database/migrations/2026_06_11_131315_create_owners_table.php.php
+++ database/migrations/2026_06_11_131315_create_owners_table.php.php
@@ -24,6 +24,10 @@ return new class extends Migration
 
             $table->timestamp('created_at')
                 ->useCurrent();
+
+            $table->timestamp('updated_at')
+                ->useCurrent()
+                ->useCurrentOnUpdate();
         });
     }
 
